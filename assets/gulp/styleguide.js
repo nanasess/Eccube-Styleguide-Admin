ddc1cc098ac2e7669b37e503ea30894df7d76c88
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-04-23 13:23:01 +0900

    feat: Migrate to Node.js v22 and Gulp 4

--- assets/gulp/styleguide.js
+++ assets/gulp/styleguide.js
@@ -1,88 +1,98 @@
-"use strict"
+"use strict";
 
-var gulp = require('gulp');
-var styleguide = require('sc5-styleguide');
-var sass = require('gulp-sass');
-var outputPath = 'public';
-const runSequence = require("run-sequence");
+const gulp = require('gulp');
+// Update require to use nanasess-sc5-styleguide
+const styleguide = require('nanasess-sc5-styleguide');
+const sassCompiler = require('sass'); // Import sass compiler
+const gulpSass = require('gulp-sass')(sassCompiler); // Pass compiler to gulp-sass
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
 
+const { src, dest } = global; // Removed scss_option as it's not used here
+const outputPath = dest; // Use global.dest
+const styleguideDest = `${outputPath}styleguide/`;
+const sassSrcPath = `${src}assets/scss/`;
 
-const {src,dest,scss_option} = global;
+const styleguideConfig = {
+    title: 'EC-CUBE Admin Styleguide',
+    // server: false, // Controlled by specific tasks
+    rootPath: styleguideDest,
+    overviewPath: `${src}styleguide.md`,
+    disableHtml5Mode: true, // Recommended for compatibility
+    disableEncapsulation: true,
+    enablePug: true,
+    extraHead: [
+        // Corrected HTML syntax and added missing quotes
+        '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">',
+        '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>',
+        '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>',
+        '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>',
+        // Use relative path from styleguideDest for custom script
+        '<script type="text/javascript" src="../js/script.js"></script>',
+        '<style>.sg-logo,.sg-design{display:none}</style>',
+        '<style>.view-index{background-color:#fff}</style>'
+    ],
+    // Remove the problematic styleguideProcessors configuration
+    // styleguideProcessors: { ... }
+};
 
-const target = src+'assets/scss/';
+// --- Styleguide Tasks (Gulp 4) ---
 
-const styelguideConfig = {
-  title: 'My Styleguide',
-  server: false,
-  rootPath: 'public/',
-  overviewPath: 'styleguide.md',
-  disableEncapsulation: true,
-  enablePug: true,
-  extraHead:[
-    '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", rel="stylesheet", integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN", crossorigin="anonymous">',
-    '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>',
-    '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>',
-    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>',
-    '<script type="text/javascript" src="/js/script.js"></script>',
-    '<style>.sg-logo,.sg-design{display:none}</style>',
-    '<style>.view-index{background-color:#fff}</style>'
-  ],
-}
-
-// サーバを生成するタスク
-gulp.task('styleguide:generate', function() {
-    var options = Object.assign({},styelguideConfig,{
-      server:true,
-      port: 4000
-    })
-    return gulp.src(target+'**/*.scss')
+// Generate task (for server)
+function styleguideGenerate() {
+    const options = { ...styleguideConfig, server: true, port: 4000, rootPath: outputPath }; // Serve from 'public'
+    return gulp.src(sassSrcPath + '**/*.scss')
+        .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
         .pipe(styleguide.generate(options))
         .pipe(gulp.dest(outputPath));
-});
+}
 
-// サーバを生成せずビルドするだけ
-gulp.task('styleguide:build:generate', function() {
-  styelguideConfig.appRoot = "/styleguide/"
-    return gulp.src(target+'**/*.scss')
-        .pipe(styleguide.generate(styelguideConfig))
-        .pipe(gulp.dest(outputPath+"/styleguide/"));
-});
-
-gulp.task('styleguide:build:applystyles', function() {
-  return gulp.src(target+'app.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(styleguide.applyStyles())
-    .pipe(gulp.dest(outputPath+"/styleguide/"));
-});
-
-
-gulp.task('styleguide:applystyles', function() {
-    const plumber = require("gulp-plumber")
-    return gulp.src(target+'app.scss')
-        .pipe(plumber())
-        .pipe(sass({
-            errLogToConsole: true
-        }))
+// Apply styles task (for server)
+function styleguideApplyStylesServer() {
+    return gulp.src(sassSrcPath + 'app.scss')
+        .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+        .pipe(gulpSass().on('error', gulpSass.logError))
         .pipe(styleguide.applyStyles())
-        .pipe(gulp.dest(outputPath));
-});
+        .pipe(gulp.dest(outputPath)); // Apply to 'public'
+}
 
-// // スタイルガイドに必要な画像を移動させる
-// gulp.task('styleguide:image', function(){
-//     return gulp.src(dest+'/image/**/*.png')
-//         .pipe(gulp.dest("./styleguide/image"));
-// });
+// Generate task (for build)
+function styleguideGenerateBuild() {
+    const options = { ...styleguideConfig, appRoot: "/styleguide/", server: false, rootPath: styleguideDest }; // Build to 'public/styleguide'
+    return gulp.src(sassSrcPath + '**/*.scss')
+        .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+        .pipe(styleguide.generate(options))
+        .pipe(gulp.dest(styleguideDest));
+}
 
-gulp.task('styleguide:build', ["styleguide:build:generate", "styleguide:build:applystyles"]);
+// Apply styles task (for build)
+function styleguideApplyStylesBuild() {
+    return gulp.src(sassSrcPath + 'app.scss')
+        .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(styleguide.applyStyles())
+        .pipe(gulp.dest(styleguideDest)); // Apply to 'public/styleguide'
+}
 
-gulp.task('styleguide:dev', ['styleguide:generate','styleguide:applystyles']);
+// Combined build task
+const styleguideBuild = gulp.parallel(styleguideGenerateBuild, styleguideApplyStylesBuild);
 
-gulp.task('styleguide:server', ['styleguide:dev'],()=>{
-    gulp.watch(target+'**/*.scss',["styleguide:dev"])
-});
+// Combined server task (generate + apply styles)
+const styleguideDev = gulp.parallel(styleguideGenerate, styleguideApplyStylesServer);
 
-gulp.task('styleguide', ['styleguide:build']);
+// Watch task
+function watchStyleguide() {
+    // Watch sass files and run the dev task on change
+    return gulp.watch(sassSrcPath + '**/*.scss', gulp.series(styleguideDev));
+}
 
-global.build.push('styleguide')
+// Removed old task definitions and global assignments
+
+// Export tasks for use in gulpfile.js
+module.exports = {
+    styleguideBuild: styleguideBuild,
+    styleguideServer: styleguideDev, // Renamed from styleguide:dev
+    styleguideGenerate: styleguideGenerate, // Export the generate task
+    styleguideApply: styleguideApplyStylesServer, // Export apply styles separately if needed
+    watchStyleguide: watchStyleguide
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,52 @@
 const gulp = require("gulp");
-const runSequence = require("run-sequence");
-
-global.watch = [];
-global.build = [];
 
 global.src = "./";
 global.dest = "./public/";
 
-require("./assets/gulp/pug.js")
-require("./assets/gulp/sass.js")
-// require("./frontend/gulp/webpack.js")
-require("./assets/gulp/styleguide.js")
-require("./assets/gulp/browserSync.js")
+// Load tasks
+const pugTasks = require("./assets/gulp/pug.js");
+const sassTasks = require("./assets/gulp/sass.js");
+// const webpackTasks = require("./assets/gulp/webpack.js"); // Keep commented for now
+const styleguideTasks = require("./assets/gulp/styleguide.js");
+const browserSyncTasks = require("./assets/gulp/browserSync.js");
 
+// --- Individual Tasks ---
 
-gulp.task("mv",()=>{
+// Simple file move task
+gulp.task("mv", () => {
   return gulp.src([
-    `${src}static/**/*`
+    `${global.src}static/**/*`
   ])
-    .pipe(gulp.dest(`${dest}`));
-})
+    .pipe(gulp.dest(`${global.dest}`));
+});
 
-gulp.task("watch",global.watch)
+// --- Composite Tasks (Gulp 4 style) ---
 
-gulp.task("build",(cb)=>{
-  global.build.push(cb)
-  runSequence.apply(this,global.build)
-})
+// Development task for MOC pages
+gulp.task('dev:moc', gulp.series(
+  (done) => { /* Add init step if needed, e.g., clean public dir */ done(); },
+  gulp.parallel(pugTasks.pug, sassTasks.sass, "mv"), // Compile Pug, Sass, move static files
+  gulp.parallel(browserSyncTasks.browserSync, pugTasks.watchPug, sassTasks.watchSass) // Start server and watch files
+));
 
-gulp.task("default",["watch"])
+// Development task for Styleguide
+gulp.task('dev:sg', gulp.series(
+  (done) => { /* Add init step if needed */ done(); },
+  gulp.parallel(pugTasks.pug, sassTasks.sass, "mv"), // Compile Pug, Sass, move static files
+  styleguideTasks.styleguideApply, // Apply styleguide specifics
+  gulp.parallel(styleguideTasks.styleguideServer, pugTasks.watchPug, sassTasks.watchSass, styleguideTasks.watchStyleguide) // Start styleguide server and watch files
+));
+
+// Combined Development task
+gulp.task('dev', gulp.parallel('dev:moc', 'dev:sg')); // Note: Running two servers might conflict on ports. Adjust browserSync config if needed.
+
+// Build task
+gulp.task("build", gulp.series(
+  (done) => { /* Add init step if needed, e.g., clean public dir */ done(); },
+  gulp.parallel(pugTasks.pug, sassTasks.sass, "mv"), // Compile Pug, Sass, move static files
+  styleguideTasks.styleguideBuild // Build styleguide
+));
+
+// Default task (alias for build)
+gulp.task("default", gulp.series("build"));
 
